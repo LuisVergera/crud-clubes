36c66017dd187e4fdd1f559a950c0c241800d121
LuisVergera <69331928+LuisVergera@users.noreply.github.com>
2022-09-10 23:33:31 -0300

    Agrega Handlebars del home
    Se muestra el listado de equipos completo, se cambian las rutas de los archivos y un archivos de estilos

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-const express = require("express");
+const fs = require('fs');
+const express = require('express');
+const multer = require('multer');
+
+const upload = multer({ dest: './uploads/imagenes' });
+const exphbs = require('express-handlebars');
 
 const PUERTO = 8080;
-
 const app = express();
+const hbs = exphbs.create();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, './src')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(`${__dirname}/uploads`));
+
+app.get('/', (req, res) => {
+  const equipos = fs.readFileSync('./data/equipos.json');
+  const clubes = JSON.parse(equipos);
+  res.render('body', {
+    layout: 'inicio',
+    data: {
+      clubes,
+    },
+  });
+});
+
+app.listen(PUERTO);
+console.log(`Escuchando en http://localhost:${PUERTO}`);
