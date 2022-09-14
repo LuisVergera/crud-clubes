078dc22e0e387b800316d7310fc4d5e74b49419a
LuisVergera <69331928+LuisVergera@users.noreply.github.com>
2022-09-14 20:57:51 -0300

    Añade la opcion de ver el detalle de los equipos y agregar uno propio
    También se agregaron sus respectivas handlebars

--- index.js
+++ index.js
@@ -18,13 +18,59 @@ app.set('view engine', 'handlebars');
 app.use(express.static(`${__dirname}/uploads`));
 
 app.get('/', (req, res) => {
-  const equipos = fs.readFileSync('./data/equipos.json');
-  const clubes = JSON.parse(equipos);
+  const dataClubes = fs.readFileSync('./data/equipos.json');
+  const clubes = JSON.parse(dataClubes);
   res.render('body', {
-    layout: 'inicio',
+    layout: 'boiler',
     data: {
       clubes,
     },
+  });
+});
+
+app.get('/club/:id', (req, res) => {
+  const dataClubes = fs.readFileSync('./data/equipos.json');
+  const clubes = JSON.parse(dataClubes);
+  const club = clubes.find((club) => club.id === Number(req.params.id));
+  res.render('club', {
+    layout: 'boiler',
+    data: {
+      club,
+    },
+  });
+});
+
+app.get('/add', (req, res) => {
+  res.render('add', {
+    layout: 'boiler',
+  });
+});
+
+app.post('/add', upload.single('crest'), (req, res) => {
+  const dataClubes = fs.readFileSync('./data/equipos.json');
+  const clubes = JSON.parse(dataClubes);
+  //const idArray = [];
+  //const unusedId = Object.keys(clubes).filter((id))
+
+  const nuevoClub = {
+    id: '',
+    area: {
+      id: '',
+      name: req.body.country,
+    },
+    name: req.body.name,
+    crestUrl: req.file,
+    address: req.body.address,
+    phone: req.body.phone,
+    founded: req.body.founded,
+    clubColors: req.body.clubColors,
+    venue: req.body.venue,
+    lastUpdated: new Date().toISOString(),
+  };
+  clubes.push(nuevoClub);
+  fs.writeFileSync('./data/equipos.json', JSON.stringify(clubes));
+  res.render('add', {
+    layout: 'boiler',
   });
 });
 
