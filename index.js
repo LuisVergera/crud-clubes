1738fe0555e66f304b9ba6e572496e310198e905
LuisVergera <69331928+LuisVergera@users.noreply.github.com>
2022-09-23 14:17:51 -0300

    Agrega funciones principales
    Dentro de ellas estan las opciones de ver, editar, agregar y borrar los equipos

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const fs = require('fs');
 const express = require('express');
 const multer = require('multer');
-
-const upload = multer({ dest: './uploads/imagenes' });
 const exphbs = require('express-handlebars');
 
 const PUERTO = 8080;
 const app = express();
 const hbs = exphbs.create();
 const path = require('path');
-
-app.use(express.static(path.join(__dirname, './src')));
-
+app.use(express.static(path.join(__dirname, '/uploads/imagenes')));
+app.use(express.static(path.join(__dirname, '/src')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.static(`${__dirname}/uploads`));
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, `${__dirname}/uploads/imagenes`);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   const dataClubes = fs.readFileSync('./data/equipos.json');
@@ -53,13 +61,13 @@ app.post('/add', upload.single('crest'), (req, res) => {
   //const unusedId = Object.keys(clubes).filter((id))
 
   const nuevoClub = {
-    id: '',
+    id: parseInt(req.body.id),
     area: {
       id: '',
       name: req.body.country,
     },
     name: req.body.name,
-    crestUrl: req.file,
+    crestUrl: req.file.filename,
     address: req.body.address,
     phone: req.body.phone,
     founded: req.body.founded,
@@ -67,11 +75,81 @@ app.post('/add', upload.single('crest'), (req, res) => {
     venue: req.body.venue,
     lastUpdated: new Date().toISOString(),
   };
+
   clubes.push(nuevoClub);
   fs.writeFileSync('./data/equipos.json', JSON.stringify(clubes));
   res.render('add', {
     layout: 'boiler',
   });
+});
+
+app.get('/edit/:id', (req, res) => {
+  const dataClubes = fs.readFileSync('./data/equipos.json');
+  const clubes = JSON.parse(dataClubes);
+  const club = clubes.find((club) => club.id === Number(req.params.id));
+
+  res.render('edit', {
+    layout: 'boiler',
+    club,
+  });
+});
+
+app.post('/edit/:id', upload.single('crest'), (req, res) => {
+  const dataClubes = fs.readFileSync('./data/equipos.json');
+  const clubes = JSON.parse(dataClubes);
+  const club = clubes.find((club) => club.id === Number(req.params.id));
+  const indexClub = clubes.findIndex(
+    (club) => club.id === Number(req.params.id)
+  );
+
+  const clubActualizado = {
+    id: parseInt(req.body.id),
+    area: {
+      id: '',
+      name: req.body.country,
+    },
+    name: req.body.name,
+    crestUrl: req.file.filename,
+    address: req.body.address,
+    phone: req.body.phone,
+    founded: req.body.founded,
+    clubColors: req.body.clubColors,
+    venue: req.body.venue,
+    lastUpdated: new Date().toISOString(),
+  };
+
+  clubes.splice(indexClub, 1, clubActualizado);
+  if (/^\.\.\/uploads\/imagenes/.test(club.crestUrl)) {
+    fs.unlinkSync(club.crestUrl.slice(3));
+    console.log('Image deleted!');
+  }
+  fs.writeFileSync(`./data/equipos.json`, JSON.stringify(clubes));
+  console.log('The file was edited successfully!');
+  res.render('edit', {
+    layout: 'boiler',
+    message: 'The team was edited successfully',
+    club,
+  });
+});
+
+app.get('/delete/:id', (req, res) => {
+  const dataClubes = fs.readFileSync('./data/equipos.json');
+  const clubes = JSON.parse(dataClubes);
+  const club = clubes.find((club) => club.id === Number(req.params.id));
+
+  const indexClubABorrar = clubes.findIndex(
+    (club) => club.id === Number(req.params.id)
+  );
+
+  const borrarClub = (clubes, indexClubABorrar) => {
+    clubes.splice(indexClubABorrar, 1);
+    return clubes;
+  };
+  const nuevoArrayClubes = borrarClub(clubes, indexClubABorrar);
+
+  fs.writeFileSync(`./data/equipos.json`, JSON.stringify(nuevoArrayClubes));
+  console.log('Club deleted successfully!');
+  res.redirect('/');
 });
 
 app.listen(PUERTO);
